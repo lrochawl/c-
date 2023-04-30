fcf5c87bc293924b842ac0cc9a071032a36c606f
Lucas Rocha <wltopos@gmail.com>
2023-04-30 19:58:24 -0300

    funções de repetição
    while()

---
+++ teste11.cpp
@@ -0,0 +1,19 @@
+//Funções de repetição
+// while()
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n;
+
+    n = 0;
+    while(n <= 100){
+        cout << "ID: " << n << endl;
+        n++;
+    }
+
+
+    return 0;
+}
