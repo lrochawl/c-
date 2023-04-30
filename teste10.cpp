ef63d833ac9024a05c530306dbb378ccc607625c
Lucas Rocha <wltopos@gmail.com>
2023-04-30 17:23:25 -0300

    teste de depuração

---
+++ teste10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int ano, idade;
+
+    cout << "Digite o ano de nascimento";
+    cin >> ano;
+
+    idade = 2023 - ano;
+    cout << "Idade: " << idade;
+}
