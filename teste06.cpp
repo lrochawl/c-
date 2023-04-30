3d9d1f65b8bdda6ea2ec5f1e3c18389470d1e1ed
Lucas Rocha <wltopos@gmail.com>
2023-04-30 16:12:45 -0300

    Operações condicionais aninhadas

--- teste06.cpp
+++ teste06.cpp
@@ -1,6 +1,7 @@
 // operadores condicionais e operadores de comparação
 // == igual, > maior, < menor, >= igual, <= menor ou igual
 // != desigualdade ou diferente de de...
+//Operações condicionais aninhadas
 #include <iostream>
 
 using namespace std;
@@ -8,9 +9,13 @@ using namespace std;
 int main()
 {
     int dinheiro = 20;
+    string nome = "Lucas";
 
     if (dinheiro > 10)
     {
+        if(nome != ""){
+            cout << nome << endl;
+        }
         cout << "Eu vou ao cinema!";
     }
     else
