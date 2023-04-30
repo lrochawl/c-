73f9e7fe2ccd5a5e95913a4bd05f58458ccc4f60
Lucas Rocha <wltopos@gmail.com>
2023-04-30 16:36:44 -0300

    switch

---
+++ teste08.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int num = 10;
+
+    switch (num)
+    {
+    case 9:
+        cout << "Numero 9";
+        break;
+    case 10:
+        cout << "Numero 10";
+        break;
+    case 11:
+        cout << "Numero 11";
+        break;
+    default:
+        cout << "Numero não localizado";
+    }
+
+    return 0;
+}
