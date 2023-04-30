2d9246ff00d3695929f123409f2185d2c02244de
Lucas Rocha <wltopos@gmail.com>
2023-04-30 15:00:10 -0300

    initial

--- teste01.cpp
+++ teste01.cpp
@@ -1,6 +1,10 @@
+//Programa que imprime Hello World!
+
 #include <iostream>
 
+using namespace std;
+
 int main() {
-  std::cout << "Hello world!\n";
+  cout << "Hello world!\n";
   return 0;
 }
---
+++ teste02.cpp
@@ -0,0 +1,12 @@
+//Criando variaveis
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int idade; //variavel que armazena uma idade
+    idade = 31;
+    cout << idade;
+    return 0;
+}
