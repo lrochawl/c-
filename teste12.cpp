472c8813753d17c47f592edbdc4223f912633ba6
Lucas Rocha <wltopos@gmail.com>
2023-04-30 21:53:22 -0300

    Estrutura de repetição For
    Usando o FOR para fatorar um numero

--- teste12.cpp
+++ teste12.cpp
@@ -1,20 +1,35 @@
 // estrutura de repetição
-//com multiplos argumentos
+// com multiplos argumentos
+
+// Calcular fatorial
+
+// !5 = 5 * 4 * 3 * 2 * 1 = 12
 #include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-    for(int i; i <= 100; i++){
-        cout << "ID: " << i << endl;
+    int num = 0, fatorar = 1, result = 1;
+
+    cout << "Digite um numero para fatorar - ";
+    cin >> num;
+
+    for (int i = 1; i < num; i++)
+    {
+        result = result * i;
     }
-    
-    for(int i, a; i <= 100; i++, a * i){
-        cout << "ID: " << i << endl;
-        cout << "ID: " << a << endl;
-    }
+    cout << "Fatorar " << result << endl;
+
+    // for(int i; i <= 100; i++){
+    //     cout << "ID: " << i << endl;
+    // }
+
+    // for(int i, a; i <= 100; i++, a * i){
+    //     cout << "ID: " << i << endl;
+    //     cout << "ID: " << a << endl;
+    // }
 
     return 0;
-
 }
