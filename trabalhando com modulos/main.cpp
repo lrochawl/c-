05636350cd8e1756fba4ece07b5f9fe08d1a2148
Lucas Rocha <wltopos@gmail.com>
2023-05-03 17:45:34 -0300

    update

---
+++ trabalhando com modulos/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "my_math.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int n = 5;
+
+    cout << "O Fatorial de " << n << ": " << fatorial(n);
+
+    return 0;
+}
---
+++ trabalhando com modulos/my_math.h
@@ -0,0 +1,8 @@
+#ifndef _MY_MATH_H_
+#define _MY_MATH_H_
+
+int fatorial(int n);
+int area_quadrado(int lado);
+int area_retangulo(int lado, int base);
+
+#endif
